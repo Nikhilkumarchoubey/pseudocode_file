df274b7f6c6e249f3f8f2c1549e5a6fb63a9d004
Nikhilkumarchoubey <choubeynikhil15@gmail.com>
2026-03-22 19:20:33 +0530

    Add files via upload

---
+++ SumOfSeries_1_to_n.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class SumOfSeries_1_to_n {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n, sum = 0;
+        System.out.print("Enter n: ");
+        n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            sum = sum + i;
+        }
+        System.out.println("Sum = " + sum);
+    sc.close();
+    }
+}
+
+/*
+DECLARE n : INTEGER
+DECLARE sum : INTEGER
+SET sum := 0
+READ n
+FOR i := 1 TO n
+    sum := sum + i
+END FOR
+PRINT sum
+*/
